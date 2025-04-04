78f0113a026f796544944e1211793097aeb46cb0
jb974 <bertil974@hotmail.fr>
2025-04-04 08:27:39 +0200

    initialisation

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bertil Portfolio</title>
+    <meta name="description" content="Bertil Portfolio">
+    <meta name="keywords" content="Bertil Portfolio">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <link rel="stylesheet" type="text/css" href="./asset/style.css">
+    <script type="text/javascript" src="./asset/main.js" defer ></script>
+</head>
+<body>
+    <div class="contenair">
+        <header>
+            <h1>developpeur web & web mobile</h1>
+        </header>
+    </div>
+</body>
