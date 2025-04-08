c9ec852bd411a445537fe46f2895a1a0958f1885
jb974 <bertil974@hotmail.fr>
2025-04-08 08:25:51 +0200

    dev structure html & css

--- public/index.php
+++ public/index.php
@@ -14,7 +14,32 @@
 <body>
     <div class="contenair">
         <header>
-            <h1>developpeur web & web mobile</h1>
+            <div class="box-btn">
+                <button class="btn">Contactez-mon</button>
+            </div>
+            <div class="box-banier">
+                <div class="hearder-banier">
+                    <h1>developpeur web & web mobile</h1>
+                </div>
+                <div class="body-banier">
+                    <p>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam architecto dolor dolores error id inventore molestiae, mollitia natus porro quam quasi quo, reprehenderit sequi tenetur voluptatem. Ex minus quia unde?</span><span>A animi aperiam assumenda autem, corporis debitis deleniti ducimus esse excepturi ipsum nemo officiis quae quasi qui quo recusandae repellat repellendus repudiandae sapiente, suscipit temporibus velit veniam vitae? Fugit, rerum!</span><span>Amet aspernatur atque dolores eaque harum magnam perspiciatis possimus, quam tenetur totam vitae voluptates! Architecto at, dolore eaque est harum ipsa natus possimus ratione repudiandae saepe. Asperiores consequatur incidunt reprehenderit!</span>
+                    </p>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,256L18.5,234.7C36.9,213,74,171,111,181.3C147.7,192,185,256,222,245.3C258.5,235,295,149,332,117.3C369.2,85,406,107,443,106.7C480,107,517,85,554,96C590.8,107,628,149,665,154.7C701.5,160,738,128,775,122.7C812.3,117,849,139,886,133.3C923.1,128,960,96,997,96C1033.8,96,1071,128,1108,144C1144.6,160,1182,160,1218,149.3C1255.4,139,1292,117,1329,133.3C1366.2,149,1403,203,1422,229.3L1440,256L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path></svg>
+
         </header>
+        <main>
+            <article>
+                <h2>mes projet</h2>
+            </article>
+            <article>
+                <h2> mes force skill</h2>
+            </article>
+        </main>
+        <footer>
+            <h3> contact </h3>
+        </footer>
     </div>
 </body>
